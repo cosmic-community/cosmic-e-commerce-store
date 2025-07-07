ee6a09ea722623e26a8cc56fc614270ae4406f60
Cosmic AI <support@cosmicjs.com>
2025-07-07 12:31:27 -0700

    remove bg

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,9 +6,6 @@ export default function Hero() {
       {/* Background gradient */}
       <div className="absolute inset-0 bg-gradient-to-br from-primary/20 via-background to-accent/20"></div>
       
-      {/* Background pattern */}
-      <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width=\"60\" height=\"60\" viewBox=\"0 0 60 60\" xmlns=\"http://www.w3.org/2000/svg\"%3E%3Cg fill=\"none\" fill-rule=\"evenodd\"%3E%3Cg fill=\"%239C92AC\" fill-opacity=\"0.1\"%3E%3Ccircle cx=\"30\" cy=\"30\" r=\"2\"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-20"></div>
-      
       {/* Main content */}
       <div className="relative z-10 text-center max-w-4xl mx-auto px-4">
         <h1 className="text-6xl md:text-8xl font-bold mb-6">
